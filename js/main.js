348adab89372c38dcbafa4255be8a61ca4ecef0c
ZhDaria <65276314+ZhDaria@users.noreply.github.com>
2021-02-16 11:20:42 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let getRandomInteger = function(min, max) {
+const getRandomInteger = function(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
@@ -22,7 +22,7 @@ let getRandomInteger = function(min, max) {
 getRandomInteger(10, 20);
 
 
-let getRandomNumber = function(min, max, digitsCount) {
+const getRandomNumber = function(min, max, digitsCount) {
 
   if (max < min) {
     alert('Нарушен порядок ввода значений "от" и "до"');
@@ -41,8 +41,90 @@ let getRandomNumber = function(min, max, digitsCount) {
   }
 
   else {
-    return (Math.random() * (max - min + 1) + min).toFixed(digitsCount);
+    return (Math.random() * (max - min + 0.00001) + min).toFixed(digitsCount);
   }
 };
 
 getRandomNumber(3, 100, 5);
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+]
+
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+]
+
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00',
+]
+
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+]
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+]
+
+const getRandomArray = function (array) {
+  let maxRandomInteger = array.length - 1;
+  let newArray = [];
+  let newArrayLength = getRandomInteger(0, maxRandomInteger);
+
+  for (let counter = 0; counter <= newArrayLength;) {
+    let keyArray = array[getRandomInteger(0, maxRandomInteger)];
+
+    if (newArray.indexOf(keyArray) === -1) {
+      newArray.push(keyArray);
+      counter++
+    }
+  }
+
+  return newArray;
+}
+
+const createAdvert = function () {
+  return {
+    author: {
+      avatar: 'img/avatars/user0' + getRandomInteger(1, 8) + '.png',
+    },
+    offer: {
+      title: 'Сдается квартира в аренду',
+      address: '35.65000, 139.70000',
+      price: getRandomNumber(1, 10000, 2),
+      type: TYPE[getRandomInteger(0, 3)],
+      rooms: getRandomInteger(1, 10),
+      guests: getRandomInteger(1, 100),
+      checkin: CHECKIN[getRandomInteger(0, 2)],
+      checkout: CHECKOUT[getRandomInteger(0, 2)],
+      features: getRandomArray(FEATURES),
+      description: 'Очень уютная и чистая квартира',
+      photos: getRandomArray(PHOTOS),
+    },
+    location: {
+      x: getRandomNumber(35.65000, 35.70000, 5),
+      y: getRandomNumber(139.70000, 139.80000, 5),
+    },
+  }
+}
+
+let adverts = [];
+
+for (let i = 0; i < 10; i++) {
+  adverts.push(createAdvert());
+}
